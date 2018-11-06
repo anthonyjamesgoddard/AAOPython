507e35350123f4a3eabf0ced978994c4fb843810
anthonyjamesgoddard <antg3254@gmail.com>
2018-11-06 17:44:19 +0000

    added some figures

--- applicationWave.py
+++ applicationWave.py
@@ -4,17 +4,12 @@ import matplotlib.pyplot as plt
 
 
 # Example data
-plt.rc('text', usetex=True)
-plt.rc('font', family='serif')
-
-plt.xlabel(r'Eigenvalue Index',fontsize=14)
-plt.ylabel(r'Eigenvalues',fontsize=16)
 A,Pinv= twave.monolithicAAOCD(96,96)
 
 w,v= np.linalg.eig(Pinv*A)
 
-plt.plot(w,'x')
-plt.savefig('tex_demo',dpi=400)
+plt.plot(w[:192],'x')
+plt.savefig('CD96')
 plt.show()
 
 
