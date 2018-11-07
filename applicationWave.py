2b5648bac3cc6fcf7077d3484b7696715bf330c4
anthonyjamesgoddard <antg3254@gmail.com>
2018-11-07 17:39:13 +0000

    update

--- applicationWave.py
+++ applicationWave.py
@@ -2,14 +2,14 @@ import temporalwave as twave
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 # Example data
-A,Pinv= twave.monolithicAAOCD(96,96)
+plt.rc('text', usetex=True)
+plt.rc('font', family='serif')
+plt.xlabel(r'Eigenvalue Index',fontsize=14)
+plt.ylabel(r'Eigenvalues',fontsize=16)
 
+A,Pinv= twave.monolithicAAOCD(32,32)
 w,v= np.linalg.eig(Pinv*A)
-
 plt.plot(w[:192],'x')
-plt.savefig('CD96')
+plt.savefig('CD',dpi=400)
 plt.show()
-
-
